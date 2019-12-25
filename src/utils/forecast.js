e66378a975d6a252c47405aab6dfe106674acb6c
tomerzaidler <tomerzaidler@gmail.com>
2019-12-25 16:29:16 +0200

    Add high and low temp to display

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback)=>{
     }
     else
     {
-      callback(undefined,`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+      callback(undefined,`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. This High today is ${response.body.daily.data[0].temperatureHigh} with a low of ${response.body.daily.data[0].temperatureLow}. There is a ${response.body.currently.precipProbability}% chance of rain.`);
     }
 
   });
