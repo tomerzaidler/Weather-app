96bfd93c842ed4e4704244a4182f683ceaa67b22
tomerzaidler <tomerzaidler@gmail.com>
2019-12-25 16:17:34 +0200

    fixed space between words

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req,res)=>{
 
 app.get('/help', (req,res)=>{
   res.render('help',{
-    helpText: 'This is some helpful text',
+    helpText: 'Welcome to the Help section',
     title: 'Help',
     name: 'Tomer Zaidler'
   })
